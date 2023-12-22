babb5db520f34df9832f765e03961c2104963518
ldgk2712 <ldgk2712@gmail.com>
2023-12-22 10:54:04 +0700

    new

--- Lab1/BTVN/2.c
+++ Lab1/BTVN/2.c
@@ -3,10 +3,29 @@
 
 void searchCharacter(char *s, char ch)
 {
-    
+    int check = 0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (ch == s[i] || ch == s[i]+32 || ch == s[i]-32)
+        {
+            printf("Found %c at %d\n", ch, i);
+            check = 1;
+        }
+    }
+    if (!check)
+    {
+        printf("Not found!\n");
+    }
 }
 
 int main()
 {
+    char s[100];
+    char ch;
+
+    scanf("%s",s);
+    scanf(" %c",&ch);
+
+    searchCharacter(s,ch);
     return 0;
 }
